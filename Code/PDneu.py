f2a83c2656c0a8ce1372e77660a721af80318d02
Isabel Giray <Isabel Giray>
2022-01-30 17:03:38 +0100

    PDneu organized

--- Code/PDneu.py
+++ Code/PDneu.py
@@ -2,10 +2,12 @@ from color import Color, NoFill
 from array import array
 from math import pi, atan2
 import numpy as np
-import drawSvg as svg
+
+
 
 def isNearZero(z, eps : float = 1.0e-8):
     return (abs(z) < eps)
+
 
 def isApproxReal(z, eps : float = 1.0e-8):
     if type(z) is int or type(z) is float:
@@ -126,8 +128,6 @@ class PDPoint(PDObject):
     def __abs__(self):
         return abs(self._point)
 
-    ###############################################
-    # TODO
 
     # Calculates inverson at unit circle
     def getInversion(self):
@@ -143,8 +143,6 @@ class PDPoint(PDObject):
         else:
             return PDPoint(self._point/abs(self._point))
 
-    #
-    ###############################################
 
     def getRealPart(self):
         return self._point.real
@@ -212,17 +210,11 @@ class PDGeodesic(PDObject):
         self.__calculate()
         self.__isdiameter()
 
-    ###############################################
-    # TODO
     def __isdiameter(self):
         if self._point1.isZero()== True or isApproxReal(self._point2 / self._point1)==True: return True
         else: return False
 
     def __calculate(self):
-
-
-        # When is the geodesic a diameter?
-        # Tip: Use the value point2 / point1 and isApproxReal() for a criterion
 
             if self.__isdiameter():
                 self._isArc = False
@@ -248,49 +240,13 @@ class PDGeodesic(PDObject):
                 n_dir = (Qinv -Q).getNormalization() *1j
 
             # find intersection of m : M + m_dir * t and n : N + n_dir * s
-            # Tip: isomorphism a+ib <--> (a,b)
-            # (solve the linear equation system [2x2])
 
                 X=N-M
                 t=(-n_dir.getImagPart()*X.getRealPart()+n_dir.getRealPart()*X.getImagPart())/(-n_dir.getImagPart()*m_dir.getRealPart()+n_dir.getRealPart()*m_dir.getImagPart())
 
-         #(1/(m_dir.getRealPart()*(-n_dir.getImagPart())-(-n_dir.getRealPart())*m_dir.getImagPart()))*array([(-n_dir.getImagPart())*X.getRealPart()+n_dir.getRealPart()*X.getImagPart()],[(-m_dir.getImagPart())*X.getRealPart()+m_dir.getRealPart()*X.getImagPart()])
-            #FIX THIS: t =1.0/mdir.getImagePart()n_dir.getRealPart()
-
                 self._center = M + m_dir * t
                 self._radius = abs(P - self._center)
-                #print(self._center)
-                #print(self._radius)
+            
 
 
-    ###############################################
 
-    def addToSVGDrawing(self, drawing : svg.Drawing):
-        if isNearZero(self._width, 0.01):
-            if self._isArc:
-                c = self._center
-                p1 = self._point1
-                p2 = self._point2
-
-                cx = c.getRealPart()
-                cy = c.getImagPart()
-
-                r = self._radius
-
-                angle1 = mod2pi(arg((p1 - c)), symmetric = False) / (2.0*pi) * 360.0
-                angle2 = mod2pi(arg((p2 - c)), symmetric = False) / (2.0*pi) * 360.0
-
-                 #magic :)
-                if ( (p2-c) * (p1-c).conjugate() ).getImagPart() < 0:
-                    angle1, angle2 = angle2, angle1
-
-                drawing.append(svg.ArcLine(cx, cy, r, -angle1, -angle2, stroke = self._edge_color.getColor(), stroke_width = 0.01, fill = self._fill_color.getColor()))
-
-            else:
-                sx = self._point1.getRealPart()
-                sy = self._point1.getImagPart()
-
-                ex = self._point2.getRealPart()
-                ey = self._point2.getImagPart()
-
-                drawing.append(svg.Line(sx, sy, ex, ey, stroke = self._edge_color.getColor(), stroke_width = 0.01, fill = self._fill_color.getColor()))
